bf59433dd5d8c6440fe85ed9de12dac4f31d4f1d
kharechko <63398664+kharechko@users.noreply.github.com>
2020-04-28 11:15:05 +0300

    Update task1.js

--- Homework_№2/task1.js
+++ Homework_№2/task1.js
@@ -2,29 +2,29 @@
 
 let total = 0
 let test = confirm('В казці завжди перемагає добро')
-if (test==true){total++}
+if (test==true){++total}
 let test2 = confirm ("прислів’я «Хто не працює, той не їсть' належить до казки 'Лисичка та Їжак' ")
-if (test2==true){total++}
+if (test2==true){++total}
 let test3 = confirm (' «Припрошує» означає запрошує' )
-if (test3==true){total++}
+if (test3==true){++total}
 let test4 = confirm(' «Добре того вчити, хто хоче все знати»? належить до казки "Колосок"')
-if (test4==true){total++}
+if (test4==true){++total}
 let test5 = confirm (' прислів’я «Яке частування, таке і дякування»? належить до казки "Лисичка та Журавель')
-if (test5==true){total++}
+if (test5==true){++total}
 let test6 = confirm(' рибці з казки "Про дівчинку Наталочку і сріблясту рибку"? допомогла Жабка')
-if (test6==true){total++}
+if (test6==true){++total}
 let test7 = confirm (' прислів’я «Друзі пізнаються в біді»? належить казці "Соловей і Жук')
-if (test7==true){total++}
+if (test7==true){++total}
 let test8 = confirm ('Автором казки «Лисичка і Журавель», є Франко')
-if (test8==true){total++}
+if (test8==true){++total}
 let test9 = confirm ('На коней чарівниця обернула щурів')
-if (test9==true){total++}
+if (test9==true){++total}
 let test10 = confirm ('Ганс Крістіан Андерсен жив у Данії')
-if (test10==true){total++}
- let summary = alert(total++);
+if (test10==true){++total}
+let summary= alert(total);
  if (total++<=10 && total++>=7 ){ alert("Молодець, не даремно читав")}
  else if (total++<=7 && total++>=4){alert("Тобі варто зробити це знову")}
- else{alert("Ехх ти.....а я думав ти читав")}
+ else {alert("Ехх ти.....а я думав ти читав")}
  
 
 
